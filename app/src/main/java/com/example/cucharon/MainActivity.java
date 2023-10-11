fc3c521c34d20363e14c1f8ede2ef0ee96f4f535
Hugo <hugopuig88@gmail.com>
2023-10-11 09:28:12 +0200

    Login actualizado a la interfaz

--- app/src/main/java/com/example/cucharon/MainActivity.java
+++ app/src/main/java/com/example/cucharon/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
 
         btnLogin = findViewById(R.id.btnLogin);
         textUsuario = findViewById(R.id.editTextUsuario);
--- app/src/main/java/com/example/cucharon/RegistroActivity.java
+++ app/src/main/java/com/example/cucharon/RegistroActivity.java
@@ -13,7 +13,7 @@ public class RegistroActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.registro_activity);
+        setContentView(R.layout.signup);
 
     }
 
