8edeb875c3e4ec8f90f7372e473fc0becf6488a7
zarlorcode <lorena.zarapo@gmail.com>
2023-10-15 16:02:57 +0200

    PosteoProducto class

---
+++ app/src/test/java/UserInteface/PosteoProducto.java
@@ -0,0 +1,11 @@
+package UserInteface;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PosteoProducto extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {}
+
+}
