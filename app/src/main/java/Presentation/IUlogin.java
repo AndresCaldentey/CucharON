fe978ea8b3b2ecb02bac7349ab8c122363503222
Hugo <hugopuig88@gmail.com>
2023-10-18 09:16:05 +0200

    Login y mainActivity arreglos

--- app/src/main/java/Presentation/IUlogin.java
+++ app/src/main/java/Presentation/IUlogin.java
@@ -38,18 +38,6 @@ public class IUlogin extends AppCompatActivity {
         textPassword = findViewById(R.id.editTextPassword);
         linkRegistro = findViewById(R.id.textViewRegistro);
 
-        /*/Comprueba el token de inicio de sesion
-        SharedPreferences sharedPreferences = getSharedPreferences("MiAppPref", Context.MODE_PRIVATE);
-
-        // Verificar si existe un token de autenticación
-        String token = sharedPreferences.getString("token", "");
-        System.out.println("El usuario " + token + " ha iniciado sesion anteriormente");
-        if (!token.isEmpty()) {
-            // El usuario ha iniciado sesión previamente, puedes permitir el acceso a la aplicación.
-            Intent intent = new Intent(MainActivity.this, IUsugerencias.class);
-            startActivity(intent);
-        }*/
-
     }
 
 
