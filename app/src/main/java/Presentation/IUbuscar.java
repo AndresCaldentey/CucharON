0dbfce2efa05331171df5549116da07d631257af
zarlorcode <lorena.zarapo@gmail.com>
2023-10-31 08:38:30 +0100

    Mapa interactivo

--- app/src/main/java/Presentation/IUbuscar.java
+++ app/src/main/java/Presentation/IUbuscar.java
@@ -1,18 +1,42 @@
 package Presentation;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.app.ActivityCompat;
+
 
 import com.example.cucharon.R;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+//import com.google.android.gms.tasks.OnSuccessListener;
 
-public class IUbuscar extends AppCompatActivity {
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
+public class IUbuscar extends AppCompatActivity implements OnMapReadyCallback {
 
+    GoogleMap mMap;
+    public static String direccion = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +44,21 @@ public class IUbuscar extends AppCompatActivity {
         setContentView(R.layout.map);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
+    }
+
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+        float zoomLevel = 15.0f;
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(39.476802,-0.3468245), zoomLevel);
+        mMap.moveCamera(cameraUpdate);
+        LatLng españa = new LatLng(39.476802,-0.3468245);
+        mMap.addMarker(new MarkerOptions().position(españa).title("UPV"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(españa));
     }
 
 
