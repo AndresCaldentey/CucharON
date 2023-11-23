d5e9440614191b4047fd1d403ee17e9684423ede
HOREPAS <horepas@etsinf.upv.es>
2023-11-23 20:19:45 +0100

    Arreglos de refactoring en IUbuscar

--- app/src/main/java/Presentation/IUbuscar.java
+++ app/src/main/java/Presentation/IUbuscar.java
@@ -1,22 +1,14 @@
 package Presentation;
 
-import android.Manifest;
-import android.content.Context;
+
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Typeface;
-import android.location.Address;
-import android.location.Geocoder;
-import android.location.Location;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Base64;
 import android.view.Gravity;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -25,7 +17,6 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
-import androidx.core.app.ActivityCompat;
 
 
 import com.example.cucharon.Producto;
@@ -38,124 +29,97 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-//import com.google.android.gms.tasks.OnSuccessListener;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 
 import Negocio.IService;
 import Negocio.Service;
 
 public class IUbuscar extends AppCompatActivity implements OnMapReadyCallback {
 
-    GoogleMap mMap;
-    public static String direccion = "";
-    IService servicio;
-    LinearLayout layoutPlatos;
-    List<Producto> listaProductos;
-    List<LatLng> posiciones;
+    private IService servicio;
+    private LinearLayout layoutPlatos;
+    private HorizontalScrollView scrollPlatos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
         servicio = Service.getService();
-
+        scrollPlatos = findViewById(R.id.vistaInfoPlatos);
         layoutPlatos = findViewById(R.id.layoutPlatos);
-        layoutPlatos.removeAllViews();
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        if(mapFragment != null) { mapFragment.getMapAsync(this); }
 
-        //Carga las posiciones de posteo de productos
-        posiciones = new ArrayList<LatLng>();
-        listaProductos = servicio.getAllProducto();
-        for(Producto p : listaProductos)
-        {
-            LatLng nuevaPos = new LatLng(p.getDireccionLatitud(), p.getDireccionLongitud());
-            if(!posiciones.contains(nuevaPos)) { posiciones.add(nuevaPos); }
-        }
-
+        layoutPlatos.removeAllViews();  //Borra las vistas de platos de prueba que hay para ver en el xml
     }
 
 
     @Override
-    public void onMapReady(@NonNull GoogleMap googleMap) {
-        mMap = googleMap;
-
+    public void onMapReady(@NonNull GoogleMap mMap) {
         //Ajustar camara inicial
         float zoomLevel = 15.0f; // Puedes ajustar este valor según tus necesidades
         LatLng posicionInicial = new LatLng(39.476802,-0.3468245);
         CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(posicionInicial, zoomLevel);
         mMap.moveCamera(cameraUpdate);
 
-        //Agregar marcadores de puntos en los que se han publicado platos
-        for(LatLng p : posiciones)
+        //Carga las posiciones de posteo de productos y agrega los marcadores en el mapa
+        List<LatLng> posiciones = new ArrayList<>();
+        for(Producto p : servicio.getAllProducto())
         {
-            Marker m = mMap.addMarker(new MarkerOptions().position(p));
-            m.setTag(p.latitude + "h" + p.longitude); //Le pongo este caracter para luego poder separar latitud de longitud
-
+            LatLng nuevaPos = new LatLng(p.getDireccionLatitud(), p.getDireccionLongitud());
+            if(!posiciones.contains(nuevaPos))
+            {
+                posiciones.add(nuevaPos);
+                Marker m = mMap.addMarker(new MarkerOptions().position(nuevaPos));
+                if(m != null) { m.setTag(nuevaPos.latitude + "h" + nuevaPos.longitude); }
+            }
         }
 
         //Se hace click en marcador
-        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-            @Override
-            public boolean onMarkerClick(Marker marker) {
-                //Se borran los productos de la seleccion anterior
-                layoutPlatos.removeAllViews();
+        mMap.setOnMarkerClickListener(marker -> {
+            layoutPlatos.removeAllViews();  // Se borran los productos de la selección anterior
+            scrollPlatos.fullScroll(View.FOCUS_LEFT);   // Se recorre el scroll hasta el inicio
 
-                //Se cargan los productos posteado en la posicion seleccionada
+            // Se cargan los productos posteado en la posición seleccionada
+            if(marker.getTag() != null)
+            {
                 String[] s = marker.getTag().toString().split("h");
                 List<Producto> lProducts = servicio.getProductosByPosicion(Double.parseDouble(s[0]), Double.parseDouble(s[1]));
 
-                //Se crea el layout de informacion de cada producto                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             plato
-                for(Producto p : lProducts) { crearInfoPlato(p); }
-
-                return false; // Devuelve 'true' si consumes el evento, 'false' si no.
+                for (Producto p : lProducts) { crearInfoPlato(p); } //Se crea el layout de información de cada producto
             }
+
+            return false; // Devuelve 'true' si consumes el evento, 'false' si no.
         });
     }
 
-    private LinearLayout crearInfoPlato(Producto producto)
+    private void crearInfoPlato(Producto producto)
     {
         LinearLayout panelPlato = new LinearLayout(this);
-
-        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                0);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.MATCH_PARENT, 0);
         layoutParams.setMarginStart(50);
         layoutParams.setMarginEnd(50);
         panelPlato.setLayoutParams(layoutParams);
-
         panelPlato.setBackgroundResource(R.drawable.fondoplatomapa);
         panelPlato.setOrientation(LinearLayout.HORIZONTAL);
-        panelPlato.setTag(producto.getIdProducto());
 
-        ImageView imagen = crearImagenProducto(producto);   //Crea la imagen
-        LinearLayout descripcion = crearDescripcion(producto);  //Crea la descripcion del producto con nombre,horario,precio
+        //Agrega la imagen y la descripcion deproducto al layout
+        panelPlato.addView(crearImagenProducto(producto));
+        panelPlato.addView(crearDescripcion(producto));
 
-        panelPlato.addView(imagen);
-        panelPlato.addView(descripcion);
+        layoutPlatos.addView(panelPlato);   // Agrega el LinearLayout principal a tu layout principal
 
-        // Agrega el LinearLayout principal a tu layout principal
-        layoutPlatos.addView(panelPlato);
-
-        //Han seleccionado un plato
-        panelPlato.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Transicion a informacion precisa del plato
-                Intent intent = new Intent(IUbuscar.this,IUreserva.class);
-                intent.putExtra("Producto",producto.getIdProducto());
-                startActivity(intent);
-            }
+        //Han seleccionado un plato y se hace la transicion a IUreserva
+        panelPlato.setOnClickListener(v -> {
+            Intent intent = new Intent(IUbuscar.this, IUreserva.class);
+            intent.putExtra("Producto", producto.getIdProducto());
+            startActivity(intent);
         });
-
-        return panelPlato;
     }
 
     private ImageView crearImagenProducto(Producto producto)
@@ -165,7 +129,6 @@ public class IUbuscar extends AppCompatActivity implements OnMapReadyCallback {
         LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(300, 300, 0);
         layoutParams.gravity = Gravity.CENTER;
         imagenProducto.setLayoutParams(layoutParams);
-        //imagenProducto.setImageResource(R.drawable.ic_launcher_background);
 
         byte[] imageBytes = Base64.decode(producto.getImagen(), Base64.DEFAULT);
         Bitmap decodedImage = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
@@ -179,53 +142,39 @@ public class IUbuscar extends AppCompatActivity implements OnMapReadyCallback {
         LinearLayout panelDescripcion = new LinearLayout(this);
 
         LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
-                800,
+                500,
                 LinearLayout.LayoutParams.MATCH_PARENT,
                 1);
         panelDescripcion.setLayoutParams(layoutParams);
-
         panelDescripcion.setOrientation(LinearLayout.VERTICAL);
 
-        //NOMBRE PRODUCTO
-        TextView nombre = new TextView(this);
-        LinearLayout.LayoutParams layoutParamsNombre = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                1);
-        nombre.setLayoutParams(layoutParamsNombre);
-        nombre.setText(producto.getNombre());
-        nombre.setTextSize(20);
-        nombre.setTypeface(null, Typeface.BOLD);
+        //Crea los textviews del nombre, horario y precio
+        TextView nombre = crearTextView(producto.getNombre(), 20, true, View.TEXT_ALIGNMENT_TEXT_START);
+        TextView horario = crearTextView(producto.getHoraRecogida(), 16, false, View.TEXT_ALIGNMENT_TEXT_START);
+        TextView precio = crearTextView(producto.getPrecio()+"€", 20, true, View.TEXT_ALIGNMENT_TEXT_END);
 
+        //Agrega los textviews del nombre, horario y precio al Layout descripcion
         panelDescripcion.addView(nombre);
-
-        //HORARIO
-        TextView horario = new TextView(this);
-        LinearLayout.LayoutParams layoutParamsHorario = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                1);
-        horario.setLayoutParams(layoutParamsHorario);
-        horario.setText(producto.getHoraRecogida());
-        horario.setTextSize(16);
-
         panelDescripcion.addView(horario);
-
-        //PRECIO
-        TextView precio = new TextView(this);
-        LinearLayout.LayoutParams layoutParamsPrecio = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                1);
-        precio.setLayoutParams(layoutParamsPrecio);
-        precio.setText(producto.getPrecio()+"");
-        precio.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_END);
-        precio.setTextSize(20);
-        precio.setTypeface(null, Typeface.BOLD);
-
         panelDescripcion.addView(precio);
 
         return panelDescripcion;
+    }
+
+    private TextView crearTextView(String texto, int size, boolean esBold, int textAlgiment)
+    {
+        TextView textView = new TextView(this);
+        LinearLayout.LayoutParams layoutParamsPrecio = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                0);
+        textView.setLayoutParams(layoutParamsPrecio);
+        textView.setTextAlignment(textAlgiment);
+        textView.setText(texto);
+        textView.setTextSize(size);
+        if(esBold) { textView.setTypeface(null, Typeface.BOLD);}
+
+        return textView;
     }
 
     public void posteoProductoOnClick(View view) {
