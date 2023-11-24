909c4236407bee8fc23d5c1eb69be5806aaac465
alerone <lopezalvarezalvaro1@gmail.com>
2023-11-24 11:06:10 +0100

    imagenes en mostrar plato

--- app/src/main/java/Presentation/AdaptadorPlato.java
+++ app/src/main/java/Presentation/AdaptadorPlato.java
@@ -3,9 +3,11 @@ import static androidx.core.content.ContextCompat.startActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -18,6 +20,9 @@ import com.example.cucharon.R;
 import java.util.ArrayList;
 import java.util.List;
 
+import Negocio.IService;
+import Negocio.Service;
+
 //El Adaptador es donde se almacena cada instancia de mostrar un plato y el holder es como se define cada instancia de mostrar plato.
 public class AdaptadorPlato extends RecyclerView.Adapter<AdaptadorPlato.AdaptadorPlatoHolder>{
 
@@ -25,9 +30,10 @@ public class AdaptadorPlato extends RecyclerView.Adapter<AdaptadorPlato.Adaptado
     List<Producto> platos = new ArrayList<>();
     Context context;
     Class clase;
-
+    IService service;
     public AdaptadorPlato(Class clase) {
         this.clase = clase;
+        service = Service.getService();
     }
     public AdaptadorPlato(List<Producto> platos) {
         this.platos = platos;
@@ -57,11 +63,13 @@ public class AdaptadorPlato extends RecyclerView.Adapter<AdaptadorPlato.Adaptado
     class AdaptadorPlatoHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         //inicializar las variables :)
         TextView nomP, recP, precioP;
+        ImageView platoI;
         public AdaptadorPlatoHolder(@NonNull View itemView) {
             super(itemView);
             nomP = itemView.findViewById(R.id.nomP);
             recP = itemView.findViewById(R.id.recP);
             precioP = itemView.findViewById(R.id.precioP);
+            platoI = itemView.findViewById(R.id.platoI);
             itemView.setOnClickListener(this);
         }
         public void imprimir(int position) {
@@ -69,6 +77,8 @@ public class AdaptadorPlato extends RecyclerView.Adapter<AdaptadorPlato.Adaptado
             nomP.setText(platos.get(position).getNombre());
             recP.setText(platos.get(position).getHoraRecogida());
             precioP.setText("" + platos.get(position).getPrecio());
+            Bitmap image = service.pasarStringAImagen(platos.get(position).getImagen());
+            platoI.setImageBitmap(image);
         }
 
         @Override
