3910d44f101189e4e4d387ac019911c3db023c7c
HOREPAS <horepas@etsinf.upv.es>
2023-12-15 11:39:23 +0100

    Ya se muestran bien la lista de reservas

--- app/src/main/java/Presentation/Cesta.java
+++ app/src/main/java/Presentation/Cesta.java
@@ -6,6 +6,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
@@ -38,8 +39,11 @@ public class Cesta extends Fragment {
         recyclerViewPrevias = view.findViewById(R.id.recyclerPreviasCesta);
 
         List<Producto> reservasEnCurso = servicio.getProductoReservadoEnCurso();
+        recyclerViewEnCurso.setLayoutManager(new GridLayoutManager(this.getContext(), 1));
         recyclerViewEnCurso.setAdapter(new AdaptadorReservasEnCurso(reservasEnCurso));
+
         List<Producto> reservasPrevias = servicio.getProductoReservadoEntregado();
+        recyclerViewPrevias.setLayoutManager(new GridLayoutManager(this.getContext(), 1));
         recyclerViewPrevias.setAdapter(new AdaptadorReservasPrevias(reservasPrevias));
     }
 
