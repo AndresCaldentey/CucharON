203d9ec124ccbccfba0151fcae968896717da0a5
zarlorcode <lorena.zarapo@gmail.com>
2023-12-20 13:23:20 +0100

    Pantalla de carga

--- app/src/main/java/Presentation/PantallaDeCargaLogin.java
+++ app/src/main/java/Presentation/PantallaDeCargaLogin.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.example.cucharon.R;
 import com.example.cucharon.Usuario;
 
@@ -21,13 +23,19 @@ public class PantallaDeCargaLogin extends AppCompatActivity {
     private static final int SPLASH_TIMEOUT = 3000; // Duración en milisegundos, tiempo que muestra la pantalla
     IService service;
     Intent intent;
+    ImageView gifImagen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.pantalla_carga);
+        setContentView(R.layout.pantalla_de_carga);
         service = Service.getService();
         intent = getIntent();
+        gifImagen = findViewById(R.id.gifFondo);
+        Glide.with(this)
+                .asGif()
+                .load(R.drawable.gift)
+                .into(gifImagen);
 
         // Aquí puedes realizar operaciones de inicialización como acceder a la base de datos
        Usuario usuario = service.getUsuarioByEmail(intent.getStringExtra("usuario"));
